2b0be45eefeccf2deaab74870c33b53f34cb5026
Daria Ogulchanskaya <55715903+Dash-hash@users.noreply.github.com>
2020-02-11 15:40:06 +0200

    Updated Settings.

--- Assets/Scripts/Models/Settings.cs
+++ Assets/Scripts/Models/Settings.cs
@@ -2,7 +2,7 @@
 {
     public static class Initial
     {
-        public static Resourses Resourses = new Resourses(500, 300, 200);
+        public static Resourсes Resourсes = new Resourсes(500, 300, 200);
         public static double PurchasePriсe = 1.5;
         public static double SalePriсe = 0.5;
         public static int PopulationLimit = 1000;
@@ -17,7 +17,7 @@
 
     public static class Expand
     {
-        public static Resourses Price = new Resourses(1000, 1000, 500);
+        public static Resourсes Price = new Resourсes(1000, 1000, 500);
         public const int WallLevelDecrease = 5;
     }
 
@@ -32,6 +32,24 @@
         public const double WorkshopProductionIncreace = 1.75 / 100;
         public const double IncomeIncrease = 0.25 / 100;
         public const double WallDefenceIncreace = 0.05;
+    }
+
+    public static class Train
+    {
+        public static Resourсes Price = new Resourсes(10, 10, 1);
+    }
+
+    public static class Units
+    {
+        public static Skills Parameters(Unit.Type type)
+        {
+            if (type == Unit.Type.Attack)
+                return new Skills(2, 5, 1);
+            else if (type == Unit.Type.Defence)
+                return new Skills(2, 2, 4);
+            else 
+                return new Skills(4, 3, 1);
+        }
     }
 }
 
