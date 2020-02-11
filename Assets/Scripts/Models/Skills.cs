07c237cffa4b96765db1a0f69924cf90d22c0930
Daria Ogulchanskaya <55715903+Dash-hash@users.noreply.github.com>
2020-02-11 15:39:41 +0200

    Added Skills.

---
+++ Assets/Scripts/Models/Skills.cs
@@ -0,0 +1,13 @@
+﻿public struct Skills
+{
+    public int Speed { get; private set; }
+    public double Attack { get; private set; }
+    public double Defence { get; private set; }
+
+    public Skills(int speed, double attack, double defence)
+    {
+        Speed = speed;
+        Attack = attack;
+        Defence = defence;
+    }
+}
