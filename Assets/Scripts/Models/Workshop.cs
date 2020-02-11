657aa7ffa9d6314ea17573729018d661d273f466
Daria Ogulchanskaya <55715903+Dash-hash@users.noreply.github.com>
2020-02-11 12:47:59 +0200

    Updated Workshop.

--- Assets/Scripts/Models/Workshop.cs
+++ Assets/Scripts/Models/Workshop.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Workshop : Building
+﻿public class Workshop : Building
 {
     public double Production { get; set; }
 
