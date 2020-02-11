5ebfe06fb5d2d27ff48849780e10e624b44a7a4a
Daria Ogulchanskaya <55715903+Dash-hash@users.noreply.github.com>
2020-02-11 17:46:04 +0200

    Updated GameComponent.

--- Assets/Scripts/Components/GameComponent.cs
+++ Assets/Scripts/Components/GameComponent.cs
@@ -1,32 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class GameComponent : MonoBehaviour
 {
-    private Game _game;
     public GameObject Grid;
 
-    void Start()
+    public int GameStepDuration;
+    public int Width;
+    public int Height;
+    public int Enemies;
+
+    private Game _game;
+
+    private void Start()
     {
-        _game = new Game(15, 15, 2);
+        _game = new Game(Width, Height, Enemies);
 
         var grid = Instantiate(Grid, transform);
         var gridComponent = grid.GetComponent<GridComponent>();
   
-        grid.transform.position = new Vector2(0, 0);
         gridComponent.Grid = _game.Grid;
 
-        InvokeRepeating("Step", 0, 4);
-    }
-
-    void Update()
-    {
+        InvokeRepeating(nameof(Step), GameStepDuration, GameStepDuration);
     }
     
-    void Step()
+    private void Step()
     { 
         foreach (var @base in _game.Bases)
             @base.Step();
