f03124b3c2a87795a206bf7957a16dfecabcfa55
Daria Ogulchanskaya <55715903+Dash-hash@users.noreply.github.com>
2020-02-11 12:47:02 +0200

    Updated ResidentialModule.

--- Assets/Scripts/Models/ResidentialModule.cs
+++ Assets/Scripts/Models/ResidentialModule.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ResidentialModule : Building
+﻿public class ResidentialModule : Building
 {
     public ResidentialModule()
     {
