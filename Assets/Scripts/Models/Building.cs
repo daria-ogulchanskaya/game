0ed4b9b067491b0d92a3401b73dcda02f1ef67f6
Daria Ogulchanskaya <55715903+Dash-hash@users.noreply.github.com>
2020-02-11 15:38:41 +0200

    Updated Building.

--- Assets/Scripts/Models/Building.cs
+++ Assets/Scripts/Models/Building.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Building 
+﻿public abstract class Building 
 {
     public int Level { get; internal set; }
     public bool Upgrading { get; internal set; }
 
     public abstract void LevelUp();
     public abstract void Upgrade();
+
+    //public Resourсes UpgradePrice()
+    //{
+    //}
 }
